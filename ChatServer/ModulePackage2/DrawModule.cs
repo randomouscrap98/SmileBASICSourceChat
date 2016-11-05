2bc58d482198795d48ac024b73216f7bc0410660
Carlos Sanchez <randomouscrap98@aol.com>
2016-11-05 05:50:06 +0000

    Added a few missing files

---
+++ ChatServer/ModulePackage2/DrawModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ModuleSystem;
+using System.Collections.Generic;
+using ChatEssentials;
+
+namespace ModulePackage2
+{
+   public class DrawModule: ModuleSystem.Module
+   {
+      private class DrawingInfo
+      {
+         public UserInfo User = null;
+         public string Drawing = "";
+         public DateTime postTime = new DateTime(0);
+      }
+
+      //private List<DrawingInfo> unsavedMessages = new List<DrawingInfo>();
+
+      public DrawModule()
+      {
+         Commands.Add(new ModuleCommand("drawsubmit", new List<CommandArgument> {
+            new CommandArgument("encodedDrawing", ArgumentType.Word)
+         }, "Submit an encoded drawing.", false));
+      }
+
+      public override string Nickname
+      {
+         get { return "draw"; }
+      }
+
+//      public override bool SaveFiles()
+//      {
+//         if(MyExtensions.MySerialize.SaveObject<List
+//      }
+
+      public override List<JSONObject> ProcessCommand(UserCommand command, UserInfo user, Dictionary<int, UserInfo> users)
+      {
+         List<JSONObject> outputs = new List<JSONObject>();
+
+         if (command.Command == "drawsubmit")
+         {
+            //unsavedMessages.Add(new DrawingInfo() { Drawing = command.Arguments[0], User = user, postTime = DateTime.Now });
+            UserMessageJSONObject drawingMessage = new UserMessageJSONObject(user, command.Arguments[0], command.tag);
+            drawingMessage.encoding = Nickname;
+            drawingMessage.spamValue = 0.50;
+            drawingMessage.SetUnspammable();
+            outputs.Add(drawingMessage);
+         }
+
+         return outputs; //base.ProcessCommand(command, user, users);
+      }
+   }
+}
+
